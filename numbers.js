8a03164de9f4c7c19fc47c6af59c9b475b2ab8d7
Victoria Beasley <iesnoth91@gmail.com>
2022-06-04 13:59:29 -0500

    still not understanding buttons and local storage

--- numbers.js
+++ numbers.js
@@ -4,46 +4,15 @@
 // display that number in the blank space of the header3 on that page
 
 function passvalues(){
-    var rating= document.getElementsByClassName("number").value;
+    var rating= document.getElementsByClassName("value").value;
     localStorage.setItem("ratingvalue",rating);
+    console.log(rating)
     return false;
 }
 
-document.querySelector(".number").addEventListener("click",
-function(){
-    console.log("I was clicked!")
-//     let isHeads = Math.random() < 0.5;
-    
-//     if(isHeads){
-//         console.log("It's heads!", headCount)
-//         //Need this below so it has an image to go back to.
-//         document.querySelector("#head-img").src = "./assets/images/head.png"
-//         document.querySelector('#outcome-header').textContent="You flipped heads!"
+function change_page(){
+    window.location.href = "thank-you.html";
+  } 
+  
 
-//         headCount++
-
-//         document.querySelector('#number-heads').textContent = headCount;
-//     } else {
-//         // console.log("It's tails!")
-//         document.querySelector("#head-img").src = "./assets/images/tails.png"
-//         document.querySelector('#outcome-header').textContent="You flipped tails!"
-
-//         tailsCount++
-
-//         document.querySelector('#number-tails').textContent = tailsCount;
-//     }
-
-//     localStorage.setItem("headCount", headCount);
-//     localStorage.setItem("tailsCount", tailsCount);
-
-//     let total = headCount + tailsCount;
-//     let percentHeads = Math.round(headCount/total * 100);
-//     let percentTails = Math.round(tailsCount/total * 100);
-
-//     document.querySelector("#percent-heads").textContent = percentHeads + "%";
-//     document.querySelector("#percent-tails").textContent = percentTails + "%";
-// 
-})
-
-
-
+document.getElementById("result").innerHTML=localStorage.getItem("ratingvalue");
