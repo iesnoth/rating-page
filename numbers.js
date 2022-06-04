10b8110b14b827386469fc870a414bfef25cbd64
Victoria Beasley <iesnoth91@gmail.com>
2022-06-04 14:15:35 -0500

    i don't get it

--- numbers.js
+++ numbers.js
@@ -3,16 +3,12 @@
 // save that number for when the user goes to the thank you page
 // display that number in the blank space of the header3 on that page
 
-function passvalues(){
-    var rating= document.getElementsByClassName("value").value;
-    localStorage.setItem("ratingvalue",rating);
-    console.log(rating)
-    return false;
-}
+// function passvalues(){
+//     var rating= document.getElementsByTagName("input").value;
+//     localStorage.setItem("ratingvalue",rating);
+//     console.log(rating)
+//     return false;
+// }
 
-function change_page(){
-    window.location.href = "thank-you.html";
-  } 
-  
 
 document.getElementById("result").innerHTML=localStorage.getItem("ratingvalue");
